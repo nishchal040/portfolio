0da64c4dd5441890fa7448cb05a0f12942297c85
nishchal <144818287+nishchal040@users.noreply.github.com>
2025-08-15 22:57:04 +0530

    Add files via upload

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,9 +7,9 @@ function Projects(){
             <p>These are the projects i have recently worked.</p>
             <div className='projects-container'>
                 <div className='project1'>
-                    <img src='./images/e-commerce.png'></img>
+                    <img src='./images/Undoshool.png'></img>
                     <h2>E-commerce Website(Front-End)</h2>
-                    <p>This e-commerce Website has a Homepage,Productspage,Contactpage. It has only front end, its not fully functional</p>
+                    <p>This Edu-tech Website. It has only front end, its not fully functional</p>
                 </div>
                 <div className='project2'>
                     <img src='./images/ecobags.png'></img>
