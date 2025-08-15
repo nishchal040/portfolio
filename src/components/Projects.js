29518cdc316592d274658f0919891cf67066d675
nishchal <144818287+nishchal040@users.noreply.github.com>
2025-08-15 23:07:25 +0530

    Add files via upload

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,16 +6,20 @@ function Projects(){
             <h1>Projects</h1>
             <p>These are the projects i have recently worked.</p>
             <div className='projects-container'>
-                <div className='project1'>
-                    <img src='./images/Undoshool.png'></img>
-                    <h2>E-commerce Website(Front-End)</h2>
-                    <p>This Edu-tech Website. It has only front end, its not fully functional</p>
-                </div>
-                <div className='project2'>
-                    <img src='./images/ecobags.png'></img>
-                    <h2>Paper Bag Price Calculator</h2>
-                    <p>This Website takes dimensitions of the paper bag and other aspects and gives price of per paperbag.This was made for the company EcoBagsFactory.</p>
-                </div>
+                <a href='https://undoschool.netlify.app/'>
+                    <div className='project1'>
+                        <img src='./images/Undoschool.png"'></img>
+                        <h2>Edu-Tech Website(Front-End)</h2>
+                        <p>This Edu-tech Website. It has only front end, its not fully functional</p>
+                    </div>
+                </a>
+                <a href='https://eco-bag-factory.vercel.app/'>
+                    <div className='project2'>
+                        <img src='./images/ecobags.png'></img>
+                        <h2>Paper Bag Price Calculator</h2>
+                        <p>This Website takes dimensitions of the paper bag and other aspects and gives price of per paperbag.This was made for the company EcoBagsFactory.</p>
+                    </div>
+                </a>
             </div>
         </section>
     );
